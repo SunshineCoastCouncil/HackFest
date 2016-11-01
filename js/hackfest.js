2c391010b3dab3f86d34f58c0e57903156e34d9f
Patrick Dwyer <pat@patdwyer.com.au>
2016-11-01 20:47:29 +1000

    New view and page setup to use standard html pages

--- js/hackfest.js
+++ js/hackfest.js
@@ -1,91 +1,54 @@
+// IE polyfill
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+        position = position || 0;
+        return this.substr(position, searchString.length) === searchString;
+    };
+}
+
 (function(hackfest, $, undefined) {
 
-    var HOMEPAGE = "homepage";
-    var HTMLPAGE = "htmlpage";
+    var pageList;
 
-    var pages = [];
-
-    var emptyViewTemplate = '<div class="view" id="{{name}}"></div>';
-    function renderEmptyViewContainer(name) {
-        $(Mustache.render(emptyViewTemplate, {name: name})).appendTo($("#content"));
+    function pageUrl(page) {
+        return encodeURI("page/" + page + ".html");
     }
 
-    var homepageBaseContent = '\
-        <div class="page-header">\
-            <img class="img-responsive" src="img/HackFest.jpg">\
-        </div>\
-        <div class="row">\
-            <div class="col-md-12">\
-            <div class="panel panel-info">\
-                <div class="panel-heading">Welcome to HackFest! Hacking for a Smarter Region</div>\
-                <div class="panel-body">\
-                <p>HackFest is your opportunity to work individually or in a \
-                team over the HackFest weekend to code a solution which meets \
-                one of the defined outcomes of the event, either a category \
-                with a problem to be solved or an initiative to be created.</p>\
-                <p>There will be great prizes on offer. Details will be \
-                announced at the start of November.</p>\
-                <b>Ideas Forum</b>\
-                <p>As part of HackFest, there is also the opportunity for you\
-                to submit ideas that can be used as categories or problems\
-                that you may think could be solved by the creation of a mobile\
-                application or solution.</p>\
-                </div>\
-            </div>\
-        </div>\
-    ';
-    var homepagePanelTemplate = '\
-        <div class="col-lg-4 col-md-6">\
-            <div class="panel panel-info">\
-                <div class="panel-heading">{{title}}</div>\
-                <div class="panel-body">{{body}}</div>\
-                <div class="panel-footer">\
-                    <button class="btn btn-default" data-page="{{title}}" data-modal="{{modal}}">View {{title}}</button>\
-                </div>\
-            </div>\
-        </div>';
-
-    function homepagePanelHtml(page) {
-        return Mustache.render(homepagePanelTemplate, page);
+    function renderHomepagePanels() {
+        var template = $("#homepagePanelTemplate").html();
+        var $panelContainer = $("#homepagePanelContainer");
+        Mustache.parse(template);
+        $.each(pageList, function(index, page) {
+            var $panel = $(Mustache.render(template, {
+                title: page
+            }));
+            $panel.appendTo($panelContainer);
+            $panel.find("div.panel-body").load(pageUrl(page) + "  #pageSummary");
+        });
     }
 
-    function homepageViewButtonClickHandler() {
-        var $this = $(this);
-        var page = $this.data("page");
-        if ($this.data("modal")) {
-            renderModal(page);
+    function renderView() {
+        $("div.view").hide();
+        var view = window.location.hash.substring(1);
+        if (view.startsWith("page-")) {
+            var $pageView = $("#pageView");
+            var pageName = view.substring(view.indexOf("-") + 1);
+            $pageView.children().remove();
+            $pageView.show();
+            $pageView.load(pageUrl(pageName) + " #pageContent");
         } else {
-            renderView(HTMLPAGE, page);
+            $("#homepageView").show();
         }
+
     }
 
-    function renderModal(page) {
-        alert(page);
-    }
-
-    function renderView(viewName, viewOptions) {
-        $(".view").hide();
-        var $view = $("#" + HOMEPAGE);
-        $view.children().remove();
-        if (viewName == HOMEPAGE) {
-            $(homepageBaseContent).appendTo($view);
-            $.each(pages, function(index, page) {
-                var $panel = $(homepagePanelHtml(page));
-                $panel.appendTo($view);
-                $panel.find("button").click(homepageViewButtonClickHandler);
-            });
-        } else if (viewName == HTMLPAGE) {
-            $view.load("page/" + encodeURIComponent(viewOptions) + ".html");
-        }
-        $view.show();
-    }
+    window.onhashchange = renderView;
 
     $(document).ready(function() {
-        renderEmptyViewContainer(HOMEPAGE);
-        renderEmptyViewContainer(HTMLPAGE);
-        $.getJSON("data/pages.json", function(data) {
-            pages = data;
-            renderView(HOMEPAGE);
+        $.getJSON("data/pages.json", function(pages) {
+            pageList = pages;
+            renderHomepagePanels();
+            renderView();
         });
     });
 
